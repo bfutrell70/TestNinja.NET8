ad842d902189fc0b3aab490845e9b82668885101
Brian Futrell <bfutrel@gmail.com>
2024-09-26 12:34:18 -0400

    test class added for ErrorLogger class

---
+++ TestNinja.NET8.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.NET8.Fundamentals;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestNinja.NET8.UnitTests
+{
+	[TestFixture]
+	public class ErrorLoggerTests
+	{
+		[Test]
+		public void Log_WhenCalled_SetTheLastErrorProperty()
+		{
+			var logger = new ErrorLogger();
+
+			logger.Log("abc");
+
+			Assert.That(logger.LastError, Is.EqualTo("abc"));
+		}
+	}
+}
