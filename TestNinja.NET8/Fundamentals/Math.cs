dd2571bdc07c1fdb5e7f13beb379b8fb6bd0bfc0
Brian Futrell <bfutrel@gmail.com>
2024-09-26 12:00:29 -0400

    line encoding changed again

--- TestNinja.NET8/Fundamentals/Math.cs
+++ TestNinja.NET8/Fundamentals/Math.cs
@@ -5,11 +5,11 @@ namespace TestNinja.NET8.Fundamentals
     public class Math
     {
         public int Add(int a, int b)
-        { 
-            return a + b;
-        }
-        
-        public int Max(int a, int b)
+        {
+			return a + b;
+		}
+
+		public int Max(int a, int b)
         {
             return (a > b) ? a : b;
         }
