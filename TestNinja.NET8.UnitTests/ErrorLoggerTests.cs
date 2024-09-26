b320db48fbd2951c1b10323f4725132f4b161994
Brian Futrell <bfutrel@gmail.com>
2024-09-26 12:48:52 -0400

    added test to check if an exception ws thrown and that an event returned a proper value

--- TestNinja.NET8.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.NET8.UnitTests/ErrorLoggerTests.cs
@@ -21,5 +21,33 @@ namespace TestNinja.NET8.UnitTests
 
 			Assert.That(logger.LastError, Is.EqualTo("abc"));
 		}
+
+		[TestCase(null)]
+		[TestCase("")]
+		[TestCase(" ")]
+		public void Log_WhenCalled_ThrowArgumentNullException(string error)
+		{
+			var logger = new ErrorLogger();
+
+			Assert.Throws<ArgumentNullException>(() => logger.Log(error));
+			// OR
+			//Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+		}
+
+		[Test]
+		public void Log_ValidError_RaiseErrorLoggedEvent()
+		{
+			var logger = new ErrorLogger();
+
+			// subscribe to the event to get the GUID
+			// sender is the source of the event
+			// args is the event arguments
+			var id = Guid.Empty;
+			logger.ErrorLogged += (sender, args) => { id = args; };
+
+			logger.Log("a");
+
+			Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+		}
 	}
 }
