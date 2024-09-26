f71841efe78deacfcb943a3a2fba0b7b10f903a3
Brian Futrell <bfutrel@gmail.com>
2024-09-26 09:46:47 -0400

    refactored code

--- TestNinja.NET8/Fundamentals/Reservation.cs
+++ TestNinja.NET8/Fundamentals/Reservation.cs
@@ -7,17 +7,6 @@
 		public bool CanBeCancelledBy(User user)
 		{
 			return (user.IsAdmin || MadeBy == user);
-			//if (user.IsAdmin)
-			//{
-			//	return true;
-			//}
-			//
-			//if (MadeBy == user)
-			//{
-			//  return true;
-			//}
-			//
-			//return false;
 		}
 	}
 
