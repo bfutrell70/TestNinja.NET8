b98d9b7856119b8914bc6f9896eada2268a76d2a
Brian Futrell <bfutrel@gmail.com>
2024-09-27 10:15:39 -0400

    added test class for Video Service - lecture 48

---
+++ TestNinja.NET8.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.NET8.Mocking;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestNinja.NET8.UnitTests.Mocking
+{
+	[TestFixture]
+	public class VideoServiceTests
+	{
+		[Test]
+		public void ReadVideoTitle_EmptyFile_ReturnError()
+		{
+			// arrange
+			var service = new VideoService();
+
+			// act
+			var result = service.ReadVideoTitle(new FakeFileReader());
+
+			// assert
+			Assert.That(result, Does.Contain("error").IgnoreCase);
+		}
+	}
+}
