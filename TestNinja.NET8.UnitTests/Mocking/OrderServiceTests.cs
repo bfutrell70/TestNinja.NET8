ea9efe13293a567024f1f253569fb3042c8b6cb7
Brian Futrell <bfutrel@gmail.com>
2024-09-27 10:57:52 -0400

    added test class for OrderService class

---
+++ TestNinja.NET8.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.NET8.Mocking;
+
+namespace TestNinja.NET8.UnitTests.Mocking
+{
+	[TestFixture]
+	public class OrderServiceTests
+	{
+		[Test]
+		public void PlaceOrder_WhenCalled_StoresTheOrder()
+		{
+			// arrange
+			var storage = new Mock<IStorage>();
+			var service = new OrderService(storage.Object);
+			var order = new Order();
+			
+			// act
+			service.PlaceOrder(order);
+
+			// assert
+			storage.Verify(s => s.Store(order));
+		}
+	}
+}
