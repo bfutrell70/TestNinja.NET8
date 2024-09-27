e0b0dd9318c23ed79146d7917a98f65d8c250d14
Brian Futrell <bfutrel@gmail.com>
2024-09-27 10:50:17 -0400

    temporary class to demonstrate how VideoService is used

---
+++ TestNinja.NET8/Mocking/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.NET8.Mocking
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			var service = new VideoService(new FileReader());
+			var title = service.ReadVideoTitle();
+		}
+	}
+}
