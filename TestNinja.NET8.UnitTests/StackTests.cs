dc53f22ce540275ca1549dfe5e955a5de2063d25
Brian Futrell <bfutrel@gmail.com>
2024-09-27 08:40:45 -0400

    split tests that asserted more than one thing into separate tests

--- TestNinja.NET8.UnitTests/StackTests.cs
+++ TestNinja.NET8.UnitTests/StackTests.cs
@@ -14,6 +14,8 @@ namespace TestNinja.NET8.UnitTests
 	{
 		/* stack:
 		 * Count property - get the count
+		 *		with empty stack should be 0
+		 *		when object is added to stack count should be 1
 		 * Push method
 		 *		throw argument null exception if a null argument is passed
 		 *		if a non-null argument is passed, added to the stack and the count should be 1
@@ -25,6 +27,7 @@ namespace TestNinja.NET8.UnitTests
 		 * Peek method
 		 *		if no items are in the list an invalid operation exception will be thrown
 		 *		if items are in the list, calling Peek will return the item in the list with the highest index
+		 *		   and the count should be the same as it was before the Peek call
 		 */
 
 		private Fundamentals.Stack<string> _stack;
@@ -48,43 +51,80 @@ namespace TestNinja.NET8.UnitTests
 		[Test]
 		public void Push_ValidArgument_CountIsOne()
 		{
-			// arrange
-
-
 			// act
 			_stack.Push("abc");
 
 			// assert
 			Assert.That(_stack.Count, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void Count_EmptyStack_IsZero()
+		{
+			// assert
+			Assert.That(_stack.Count, Is.EqualTo(0));
 		}
 
 		[Test]
 		public void Pop_NoItemsInList_ThrowsInvalidOperationException()
 		{
-			// arrange
-
-
-			// act
-
-
 			// assert
 			Assert.That(() => _stack.Pop(), Throws.InvalidOperationException);
 		}
 
 		[Test]
-		public void Pop_TwoItemsInList_CountIsOne()
+		public void Pop_StackWithTwoObjects_ReturnObjectFromTheTop()
 		{
+			// Mosh separated the tests for returning the last object
+			// and verifying the count - each test should be for a
+			// single item
+
 			// arrange
-			_stack.Push("abc");
-			_stack.Push("def");
+			_stack.Push("a");
+			_stack.Push("b");
+			_stack.Push("c");
 
 			// act
 			var result = _stack.Pop();
 
 			// assert
-			Assert.That(result, Is.EqualTo("def"));
-			Assert.That(_stack.Count, Is.EqualTo(1));
+			Assert.That(result, Is.EqualTo("c"));
 		}
+
+		[Test]
+		public void Pop_StackWithTwoObjects_RemoveObjectFromTheTop()
+		{
+			// Mosh separated the tests for returning the last object
+			// and verifying the count so that each test is verifying
+			// a single item
+
+			// arrange
+			_stack.Push("a");
+			_stack.Push("b");
+			_stack.Push("c");
+
+			// act
+			_stack.Pop();
+
+			// assert
+			Assert.That(_stack.Count, Is.EqualTo(2));
+		}
+
+		// my original test - tests two things
+		// [Test]
+		// public void Pop_TwoItemsInList_CountIsOne()
+		// {
+		// 	// arrange
+		// 	_stack.Push("abc");
+		// 	_stack.Push("def");
+
+		// 	// act
+		// 	var result = _stack.Pop();
+
+		// 	// assert
+		// 	Assert.That(result, Is.EqualTo("def"));
+		// 	Assert.That(_stack.Count, Is.EqualTo(1));
+		// }
 
 		[Test]
 		public void Pop_TwoItemsInListPopCalled3Times_ThrowsInvalidOperationException()
@@ -104,28 +144,60 @@ namespace TestNinja.NET8.UnitTests
 		[Test]
 		public void Peek_NoItemsInList_ThrowsInvalidOperationException()
 		{
-			// arrange
-
-
-			// act
-
-
 			// assert
 			Assert.That(() => _stack.Peek(), Throws.InvalidOperationException);
 		}
 
 		[Test]
-		public void Peek_TwoItemsInList_ReturnsLastItemAndCountIsTwo()
+		public void Peek_TwoItemsInList_DoesNotRemoveObjectOnTopOfTheStack()
 		{
+			// Mosh separates object check and count check since each unit test
+			// should test for one thing
+
 			// arrange
-			_stack.Push("abc");
-			_stack.Push("def");
+			_stack.Push("a");
+			_stack.Push("b");
+			_stack.Push("c");
+
+			// act
+			_stack.Peek();
+
+			// assert
+			Assert.That(_stack.Count, Is.EqualTo(3));
+		}
+
+		[Test]
+		public void Peek_TwoItemsInList_ReturnsObjectOnTopOfTheStack()
+		{
+			// Mosh separates object check and count check since each unit test
+			// should test for one thing
+			
+			// arrange
+			_stack.Push("a");
+			_stack.Push("b");
+			_stack.Push("c");
 
 			// act
 			var result = _stack.Peek();
 
 			// assert
-			Assert.That(result, Is.EqualTo("def"));
+			Assert.That(result, Is.EqualTo("c"));
 		}
+
+		// My original codde
+		// [Test]
+		// public void Peek_TwoItemsInList_ReturnsLastItemAndCountIsTwo()
+		// {
+		// 	// arrange
+		// 	_stack.Push("abc");
+		// 	_stack.Push("def");
+
+		// 	// act
+		// 	var result = _stack.Peek();
+
+		// 	// assert
+		// 	Assert.That(result, Is.EqualTo("def"));
+		// 	Assert.That(_stack.Count, Is.EqualTo(2));
+		// }
 	}
 }
